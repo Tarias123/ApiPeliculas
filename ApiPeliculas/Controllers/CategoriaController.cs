94ad71d4843942af149a7fe13b5c17301f64dd11
angel tapia <angeltapia1808@gmail.com>
2025-12-12 18:37:15 -0500

    Crear los controlles categoria y mapear los modelos y los Dto :)

---
+++ ApiPeliculas/Controllers/CategoriaController.cs
@@ -0,0 +1,38 @@
+﻿using ApiPeliculas.Models.Dtos;
+using ApiPeliculas.Repository.ItRepository;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPeliculas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly ICategoriaRepository _CatRepo;
+        private readonly IMapper _mapper;
+
+        public CategoriaController(ICategoriaRepository CatRepo, IMapper mapper)
+        {
+            _CatRepo = CatRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetCategorias()
+        {
+            var listaCategorias = _CatRepo.GetCategorias();
+            var listaCategoriasDto = new List<CategoriaDto>();
+            foreach (var categoria in listaCategorias)
+            {
+                listaCategoriasDto.Add(_mapper.Map<CategoriaDto>(categoria));
+            }
+            return Ok(listaCategoriasDto);
+        }
+
+
+    }
+}
--- ApiPeliculas/Program.cs
+++ ApiPeliculas/Program.cs
@@ -1,4 +1,7 @@
 using ApiPeliculas.Data;
+using ApiPeliculas.PeliculasMapper;
+using ApiPeliculas.Repository;
+using ApiPeliculas.Repository.ItRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +18,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Agregamos repositorios
+builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+// Configuramos AutoMapper
+
+// Esta es la forma correcta si PeliculasMapper está en el proyecto de arranque
+builder.Services.AddAutoMapper(typeof(PeliculasMapper));
+
 
 var app = builder.Build();
 
